310fe49952c55987ca76864ad0e07d582de8af8c
Electross <jarrettdh24@gmail.com>
2025-04-05 19:12:14 -0400

    Fatter +more rounded circle

--- CircleParticleGenerator.java
+++ CircleParticleGenerator.java
@@ -19,22 +19,31 @@ public class CircleParticleGenerator
 
     private void BuildACircle(String particle, double radius)
     {
+        //For rounding to two decimal places
         DecimalFormat df = new DecimalFormat("#.##");
         df.setRoundingMode(RoundingMode.CEILING);
 
         double xVal = radius;
         double yVal = 0;
-        double decrement = xVal * 0.1;
+        double decrement = xVal * 0.05; //Decrements the x coordinate
+        int count = 0; //Used for adjusting the decrement value to ensure circle is well-rounded
         System.out.println("particle minecraft:" + particle + " ~" + yVal + " ~ ~" + xVal + " 0 0 0 0 0");
         System.out.println("particle minecraft:" + particle + " ~" + yVal + " ~ ~" + (xVal * -1) + " 0 0 0 0 0");
         while(xVal > radius * -1) 
         {
-            xVal = xVal - decrement;
-            yVal = Math.sqrt(Math.pow(radius,2) - Math.pow(xVal,2));
-            if(Math.pow(radius,2) - Math.pow(xVal,2) < 0)
-                return;
+            if(count <= 2 || count >= 39)
+                xVal = xVal - (decrement * 0.7);
+            else 
+                xVal = xVal - decrement;
+            yVal = Math.sqrt(Math.pow(radius,2) - Math.pow(xVal,2)); //Calculates y coordinate
+            if(Math.pow(radius,2) - Math.pow(xVal,2) < 0) //Ensures y is not NaaN
+                continue;
+
+            //Prints out minecraft particle commands
             System.out.println("particle minecraft:" + particle + " ~" + df.format(yVal) + " ~ ~" + df.format(xVal) + " 0 0 0 0 0");
             System.out.println("particle minecraft:" + particle + " ~" + df.format(yVal * -1) + " ~ ~" + df.format(xVal) + " 0 0 0 0 0");
+            count++;
         }
+        System.out.println(count);
     }
 }
